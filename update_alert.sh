5cea54b7c028a5dee4327e93d45197508390a22f
XAGSolutions <46761745+XAGSolutions@users.noreply.github.com>
2023-04-05 16:30:17 -0700

    Update update_alert.sh

--- update_alert.sh
+++ update_alert.sh
@@ -11,7 +11,7 @@ icon_path="/Users/Shared/XAG_XLogomark_Digital_XAGGreen.png"
 osascript -e "display notification \"$message\" with title \"$title\""
 
 # Prompt the user to reboot the machine
-button_pressed=$(osascript -e "tell app \"System Events\" to display dialog \"XAG Solutions has delivered some critical software updates for this machine.  Please save anything that you are working on, close all applications, and then click Restart Now below.\" buttons {\"Restart Now\", \"Later\"} default button \"Restart Now\" with icon POSIX file \"$icon_path\" position {" & upperRightX & ", " & upperRightY & "}")
+button_pressed=$(osascript -e "tell app \"System Events\" to display dialog \"XAG Solutions has delivered some critical software updates for this machine.  Please save anything that you are working on, close all applications, and then click Restart Now below.\" buttons {\"Restart Now\", \"Later\"} default button \"Restart Now\" with icon POSIX file \"$icon_path\" position {100,100}")
 # Check which button was pressed
 if [[ "$button_pressed" == "button returned:Restart Now" ]]; then
     # Restart the machine
